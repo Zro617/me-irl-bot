0f46c71b323308daab1753f76bd41a805bf2bee8
Zro <zropotato@gmail.com>
2018-03-28 20:03:52 -0500

    jimp stuff + images

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discordie = require('discordie')
 const request   = require('request')
 const fs        = require('fs')
 const notifier  = require('node-notifier')
+const Jimp      = require('jimp')
 const {token}   = require('./auth.json')
 
 const OOF = `
@@ -143,7 +144,7 @@ function wordcube(message) {
 }
 
 var MeIrlBot = new Discordie()
-MeIrlBot.connect({token,autorun:true})
+MeIrlBot.connect({token,autorun:true,autoReconnect:true})
 
 MeIrlBot.Dispatcher.on('GATEWAY_READY', function () {
 	console.log('MeIrlBot connected.')
@@ -151,11 +152,12 @@ MeIrlBot.Dispatcher.on('GATEWAY_READY', function () {
 })
 
 var lastPingSent = 0
+var typing = null
 
 MeIrlBot.Dispatcher.on('MESSAGE_CREATE', function (e) {
-	const message = e.message.content;
-	const user = e.message.author;
-	const channel = e.message.channel;
+	const message = e.message.content
+	const user = e.message.author
+	const channel = e.message.channel
 	
 	if (user.id == '426776148962181120') return;
 	
@@ -187,7 +189,13 @@ MeIrlBot.Dispatcher.on('MESSAGE_CREATE', function (e) {
 		channel.sendMessage('no u')
 		
 	} else if (/no u/i.test(message)) {
-		channel.uploadFile(fs.readFileSync("C:/Users/Zachary/Pictures/cards/unoreverse.jpg"), "no_u.jpg")
+		channel.uploadFile(fs.readFileSync("./me-irl-bot/images/unoreverse.jpg"))
+		
+	} else if (e.message.attachments && e.message.attachments.some(function (a) {
+		return isImage(a.url) && /reverse|no_u/i.test(a.url)
+	})) {
+		channel.sendMessage('no, ***u***')
+		channel.uploadFile(fs.readFileSync("./me-irl-bot/images/ReverseofReverse.jpg"))
 		
 //	} else if (/press \w to pay respects/i.test(message)) {
 //		var letter = message.match(/press (\w) to pay respects/i)[1].toLowerCase()
@@ -201,10 +209,11 @@ MeIrlBot.Dispatcher.on('MESSAGE_CREATE', function (e) {
 		var now = Date.now()
 		if (user.id == '138832637258104832') {
 			channel.sendMessage('but you are zro \\🤔')
-		} else if (zro.status == 'online') {
-			channel.sendMessage('<@138832637258104832>')
 		} else if (now - lastPingSent < 60000) {
 			channel.sendMessage('you already pinged zro dummy')
+		} else if (zro.status == 'online') {
+			lastPingSent = now
+			channel.sendMessage('<@138832637258104832>')
 		} else {
 			lastPingSent = now
 			channel.sendMessage('pinging zro')
@@ -213,9 +222,97 @@ MeIrlBot.Dispatcher.on('MESSAGE_CREATE', function (e) {
 				message: user.username + ' wants your attention',
 				wait: true
 			}, function (err, res) {
-				if (err) console.error(err)
+				if (err) error(err)
 				else console.log('ping by ' + user.username)
 			})
 		}
+	} else if (/delet this/i.test(message)) {
+		channel.fetchMessages(20).then(() => {
+			for (var m of channel.messages) {
+				if (m.author.id == MeIrlBot.id) {
+					return m
+				}
+			}
+			throw 'i don\'t know what you want me to delete'
+		}).then(m => m.delete())
+		.catch(error)
+	} else if (/needs more jpe?g/i.test(message)) {
+		getFirstImage()
+		.then(imageUrl => {
+			console.log('jpegging ' + imageUrl)
+			startTyping()
+			return Jimp.read(imageUrl)
+		}).then(image => {
+			image.posterize(Math.floor(20 + 50 * Math.random()))
+			.quality(Math.floor(5 * Math.random()))
+			.getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+				clearInterval(typing)
+				if (err) throw err
+				else channel.uploadFile(buffer, 'needs_more_jpeg.jpg')
+			})
+		}).catch(error)
+	} else if (/needs more (deep)?frying/i.test(message)) {
+		getFirstImage()
+		.then(imageUrl => {
+			console.log('deep frying ' + imageUrl)
+			startTyping()
+			return Jimp.read(imageUrl)
+		}).then(image => {
+			image.contrast(Math.random())
+			.color([{apply: 'saturate', params: [Math.floor(100*Math.random())]}])
+			.posterize(Math.floor(4 + 10*Math.random()))
+			.quality(Math.floor(50*Math.random()))
+			.getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+				stopTyping()
+				if (err) throw err
+				else channel.uploadFile(buffer, 'deepfry.jpg')
+			})
+		}).catch(error)
+	} else if (/wednesday my dudes/i.test(message)) {
+		if (new Date().getDay() == 3) {
+			channel.uploadFile(fs.readFileSync('./me-irl-bot/images/wednesday.jpg'))
+		} else {
+			channel.sendMessage('no it\'s not')
+		}
+	} else if (/several people are typing/i.test(message)) {
+		MeIrlBot.User.setUsername('several people')
+		startTyping()
+		setTimeout(() => {
+			stopTyping()
+			MeIrlBot.User.setUsername('MeIRLBot')
+		}, 30000)
+	}
+	
+	function getFirstImage() {
+		return channel.fetchMessages(20).then(() => {
+			for (var m of channel.messages.reverse()) {
+				//console.log(m.embeds,m.attachments)
+				if (m.embeds && m.embeds.length && m.embeds[0].type == 'image' && isImage(m.embeds[0].url)) {
+					return m.embeds[0].url
+				} else if (m.attachments && m.attachments.length && isImage(m.attachments[0].url)) {
+					return m.attachments[0].url
+				}
+			}
+			throw 'no image found'
+		})
+	}
+	function isImage(url) {
+		return /png|jpg|jpeg$/i.test(url)
+	}
+	function error(err) {
+		console.log(err)
+		channel.sendMessage(':x: nope :no_good: :triumph::' + err + ' :pensive:')
+	}
+	function startTyping() {
+		console.log('typing...')
+		channel.sendTyping()
+		typing = setInterval(() => {
+			console.log('typing some more...')
+			channel.sendTyping()
+		}, 10000)
+	}
+	function stopTyping() {
+		console.log('stopped typing')
+		clearInterval(typing)
 	}
 })
